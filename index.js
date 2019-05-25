0357a1d905617f2c21f471620cf5500d0e78d61f
Wesley Thi <wesleyj.thi@gmail.com>
2019-05-25 00:33:34 -0700

    fixed debug

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ $(function() {
    
    $('.shopping-list').on ('click', '.shopping-item-toggle', function(event) {
     
-   $(this).closest('li').find('.shopping-item')toggleClass('shopping-item__checked');
+   $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
    
   });
 
